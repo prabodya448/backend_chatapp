5785090d59114a3ef0ac8e0d318d4569997c6fc4
prabodya <116181860+prabodya448@users.noreply.github.com>
2025-02-25 11:19:58 +0530

    fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Frontend URL
+    origin: "*", // Frontend URL
     methods: ["GET", "POST"],
   },
 });
